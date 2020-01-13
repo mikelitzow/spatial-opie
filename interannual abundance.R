234e3fe183d1eaae5c96c75b5744807974b5046a
mikelitzow <litzow@faralloninstitute.org>
2020-01-13 09:41:58 -0900

    Comparing different error structures

--- interannual abundance.R
+++ interannual abundance.R
@@ -4,7 +4,7 @@ library(nlme)
 
 # compare cpue for different sex/size categories in 2010, 2017-2019 in EBS & NBS
 
-dat <- read.csv("data/opie.data.csv")
+dat <- read.csv("data/opie.data.2.csv")
 head(dat)
 
 # create NBS/EBS code!
@@ -26,7 +26,12 @@ NBS <- dat %>%
 hist(NBS$MALE_31TO60)
 
 # better transform!
+# first question- is this the correct way to nest lat and long in the correlation structure?
+# the two are not independent, so I figured form=~LATITUDE + LONGITUDE wouldn't be right
+m1 <- gls(MALE_31TO60^0.25 ~ SURVEY_YEAR, correlation=corGaus(form=~LATITUDE / LONGITUDE), data=NBS)  
+m2 <- gls(MALE_31TO60^0.25 ~ SURVEY_YEAR, correlation=corGaus(form=~LATITUDE : LONGITUDE), data=NBS)  
 
-m1 <- gls(MALE_31TO60^0.25 ~ SURVEY_YEAR, correlation=corGaus(form=~LATITUDE / LONGITUDE), data=NBS)
 summary(m1)
-allCoef(m1)
+summary(m2)
+
+coef(m1); coef(m2)
